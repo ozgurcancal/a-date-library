9ecbf7467c41e15ba8f252286b28d44ec742c123
özgür <44574065+ozgurcancal@users.noreply.github.com>
2020-04-06 20:58:18 +0300

    Add files via upload

---
+++ date.cpp
@@ -0,0 +1,421 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <iostream>
+#include<iomanip>
+#include "Date.h"
+#include <sstream>
+#include<ctime>
+#include <map>
+#include <cstdlib>
+#include <chrono>
+#include <locale.h>
+
+const int Date::md[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+const std::string Date::my[12] = { "ocak","subat","mart","nisan","mayis","haziran","temmuz","agustos","eylul","ekim","kasim","aralik" };
+
+Date::Date() {
+
+	day = 1;
+	month = 2;
+	year = 1900;
+
+}
+
+Date::Date(int d, int m, int y) {
+	day = d;
+	month = m;
+	year = y;
+}
+
+Date::Date(const char* input) {
+	if (std::sscanf(input, "%d/%d/%d", &day, &month, &year));
+	else
+		std::cout << "pars ederken hata meydana geldi!!";
+
+}
+
+Date::Date(std::time_t timer) {
+
+	day = ptr->tm_mday;
+	month = ptr->tm_mon + 1;
+	year = ptr->tm_year;
+}
+
+int Date::get_month_day() const {
+
+	return (day);
+}
+
+int Date::get_month() const {
+
+	return(month);
+}
+
+int Date::get_year() const {
+
+	return(year);
+}
+
+int Date::get_year_day() const {
+
+	int d = day;
+	int m = month;
+
+	while (--m) {
+
+		d += md[month];
+
+	}
+	return d;
+}
+
+
+Date::WeekDay Date::get_week_day() const {
+
+	int i = ptr->tm_wday;
+
+	std::map<int, WeekDay> m;
+	m[0] = WeekDay::Sunday;
+	m[1] = WeekDay::Monday;
+	m[2] = WeekDay::Tuesday;
+	m[3] = WeekDay::Wednesday;
+	m[4] = WeekDay::Thursday;
+	m[5] = WeekDay::Friday;
+	m[6] = WeekDay::Saturday;
+
+
+	WeekDay a = m[i];
+
+	return a;
+
+}
+
+Date& Date::set_month_day(int d) {
+
+	day = d;
+	return *this;
+}
+
+Date& Date::set_month(int m) {
+
+	month = m;
+	return *this;
+}
+
+Date& Date::set_year(int y) {
+
+	year = y;
+	return *this;
+}
+
+Date& Date::set(int d, int m, int y) {
+
+	day = d;
+	month = m;
+	year = y;
+	return *this;
+}
+
+
+void Date::print_date() {
+
+	std::printf("%02d-%02d-%04d ", day, month, year);  
+}
+
+
+Date& Date::gun_cikar(Date& date, int x)const {
+
+
+	int& gün = date.day; int& ay = date.month; int& yıl = date.year;
+
+	gün = gün - x;
+
+	while (gün < 1) {
+		ay--;
+		while (ay < 1) {
+			yıl--;
+			ay = 12 + ay;
+		}
+		gün = md[ay - 1] + gün;
+
+	}
+
+	return date;
+}
+
+Date& Date::gun_topla(Date& date, int x)const {     
+
+
+	int& gün = date.day; int& ay = date.month; int& yıl = date.year;
+
+	gün = gün + x;
+
+	if (ay > 0) {
+
+		while (gün > md[ay - 1]) {
+
+			gün = gün - md[ay - 1];
+			ay++;
+
+			while (ay > 12)
+			{
+				ay = 12 - ay;
+				yıl++;
+			}
+		}
+	}
+	return date;
+
+}
+
+int Date::month_to_date(int month)const {
+
+	int day = 0;
+	while (month) {
+		day += md[month];
+		month--;
+	}
+
+	return day;
+}
+
+int Date::number_of_leapyear()const {
+	int count = 0;
+	int y = year;
+	while (y--) {
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400)) {
+
+			count++;
+		}
+	}
+	return count;
+}
+
+Date Date:: operator-(int x) {
+
+
+	return (gun_cikar(*this, x));
+}
+
+Date& Date:: operator += (int x) {
+
+
+	return gun_topla(*this, x);
+}
+
+Date& Date:: operator-=(int x) {
+
+
+	return gun_cikar(*this, x);
+}
+
+Date& Date:: operator++() {  //ön ek
+
+
+	return gun_topla(*this, 1);
+}
+
+Date Date::operator++(int) {  //son ek    
+
+	Date temp{ *this };
+
+	gun_topla(*this, 1);
+
+	return temp;
+}
+
+Date& Date:: operator--() {
+
+
+	return gun_cikar(*this, 1);
+}
+
+Date Date:: operator--(int) {
+
+	Date temp{ *this };
+
+	gun_cikar(*this, 1);
+
+
+	return temp;
+}
+
+Date Date::random_date() {
+
+
+	Date rast;
+
+	rast.year = (rand() % 80) + rast.random_min_year;
+	rast.month = (rand() % 12) + 1;
+	int day = rast.md[rast.month - 1];  //random belirlenen aydaki gün sayısı
+	rast.day = (rand() % day) + 1;
+
+	return rast;
+}
+
+constexpr bool Date::isleap(int year) {
+
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400)) {
+		std::cout << "girdiginiz yil bir artik yildir";
+		return 1;
+	}
+	else {
+		std::cout << "girdiginiz yil artik yil degil";
+		return 0;
+	}
+}
+
+
+bool operator<(const Date& self, const Date& obj) {
+
+	if (self.year < obj.year)
+		return 1;
+	else if (self.year == obj.year && self.month < obj.month)
+		return 1;
+	else if (self.year == obj.year && self.month == obj.month && self.day < obj.day)
+		return 1;
+	else
+		return 0;
+}
+
+bool operator<=(const Date& self, const Date& obj) {
+
+	return !(obj < self);
+}
+
+bool operator>(const Date& self, const Date& obj) {
+
+	return obj < self;
+}
+
+bool operator>=(const Date& self, const Date& obj) {
+
+	return !(self < obj);
+}
+
+bool operator==(const Date& self, const Date& obj) {
+
+	if (self.year == obj.year && self.month == obj.month && self.day == obj.day)
+		return 1;
+	else
+		return 0;
+}
+
+bool operator!=(const Date& self, const Date& obj) {
+
+	return !(self == obj);
+}
+
+int operator-(const Date& self, const Date& obj) {  //iki tarihi çıkaran fonksiyon
+
+	if (self > obj) {  //eğer bizim tarihimiz çıkarılan tarihten büyükse güne çevirilmiş tarihleri birbirinden çıkar.
+
+		int day = (self.year - obj.year) * 365 + self.number_of_leapyear() - obj.number_of_leapyear() + (self.month_to_date(self.month) - obj.month_to_date(obj.month)) + (self.day - obj.day);
+
+		std::cout << "iki tarih arasinda " << day << " gun fark var";
+
+		return day;
+	}
+
+	else {
+		std::cout << "ikinci deger daha buyuk hata degeri: ";
+		return -1;  //hata degeri olarak -1 donduruldu
+	}
+
+}
+
+Date operator+(Date& date, int n) {   // tarihten n gün sonrasını veren fonksiyon
+
+
+	return date.gun_topla(date, n);
+}
+
+
+Date operator+(int n, Date& date) {
+
+	return date.gun_topla(date, n);
+}
+
+
+Date::WeekDay& weekday_arttır(Date::WeekDay& r) {
+
+	int a = (static_cast<int>(r) + 1);
+	if (a > 6)
+		a = a % 7;
+	r = static_cast<Date::WeekDay>(a);
+	return r;
+
+}
+Date::WeekDay& weekday_azalt(Date::WeekDay& r) {
+
+	int a = (static_cast<int>(r) - 1);
+	if (a < 0)
+		a += 7;
+	r = static_cast<Date::WeekDay>(a);
+	return r;
+
+}
+
+Date::WeekDay& operator++(Date::WeekDay& r) {  
+
+	return weekday_arttır(r);
+}
+
+Date::WeekDay& operator++(Date::WeekDay& r, int) {
+
+	Date::WeekDay static temp{ r };
+
+	weekday_arttır(r);
+
+	return temp;
+
+}
+Date::WeekDay& operator--(Date::WeekDay& r) {
+
+
+	return weekday_azalt(r);
+
+}
+Date::WeekDay& operator--(Date::WeekDay& r, int) {
+
+	Date::WeekDay static temp{ r };
+
+	weekday_azalt(r);
+
+	return temp;
+
+}
+
+std::ostream& operator<<(std::ostream& os, const Date::WeekDay& f) { 
+
+
+	int Val = static_cast<int>(f);
+
+	switch (Val) {
+	case 0:return os << "Pazar";
+	case 1:return os << "Pazartesi";
+	case 2:return os << "Salı";
+	case 3:return os << "Çarşamba";
+	case 4:return os << "Perşembe";
+	case 5:return os << "Cuma";
+	case 6:return os << "Cumartesi";
+	default: return os << Val;
+	}
+}
+
+std::ostream& operator << (std::ostream& os, const Date& d) {
+
+	os << d.day << " " << d.my[d.month - 1] << " " << d.year << " " << d.get_week_day();
+
+	return os;
+}
+
+std::istream& operator >> (std::istream& is, Date& d) {
+
+	char c = '/';
+	is >> d.day >> c >> d.month >> c >> d.year;
+
+	return is;
+}
---
+++ date.h
@@ -0,0 +1,108 @@
+﻿#pragma once
+#ifndef DATE_H
+#define DATE_H
+#include <iosfwd>
+#include <ctime>
+
+
+
+class Date {
+private:
+	int day = 0;
+	int month = 0;
+	int year = 0;
+	time_t timer = time(NULL);
+
+	static const int md[12];
+	static const std::string my[12];
+
+
+public:
+
+	struct tm* ptr = localtime(&timer);
+
+	static constexpr int year_base = 1900;  //1		sınıfın hizmet verdiği en küçük yıl değeri
+	static constexpr int random_min_year = 1940;  //2	random_date işlevinin üreteceği tarih için en küçük yıl değeri		  
+	static constexpr int random_max_year = 2020;  //3	random_date işlevinin üreteceği tarih için en büyük yıl değeri
+	enum class WeekDay { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }; //4	Haftanın günü için enum class türü
+
+
+
+
+	Date(); //5		Varsayılan kurucu işlev: Date nesnesini 01-01-1900 tarihi ile oluşturacak
+	Date(int d, int m, int y);  //6		Date nesnesini gün, ay, yıl değeri ile oluşturacak kurucu işlev
+	Date(const char* p);  //7		Date nesnesini formatlanmış yazıdan alacağı tarih değeri ile oluşturacak. Format: gg/aa/yil
+	Date(std::time_t timer); //8	Date nesnesini "calender time" değerinden dönüştüreceği tarih değeri ile oluşturacak.
+
+
+	int get_month_day()const; //9	Ayın gününü döndürüyor.
+	int get_month()const; //10		Ay değerini döndürüyor. (Ocak 1, Şubat 2, ...)  
+	int get_year()const; //11		Tarihin yıl değerini döndürüyor
+	int get_year_day()const; //12	Yılın gününü döndürüyor (01 Ocak ---> 1 31 Aralık---> 365 ya da 366  
+
+
+	WeekDay get_week_day()const; //13	Haftanın gününü döndürüyor.
+
+	Date& set_month_day(int day); //14	Tarihin ayın günü değerini değiştiriyor.
+	Date& set_month(int month); //15	Tarihin ayını değiştiriyor
+
+
+	Date& set_year(int year); //16	Tarihin yılını değiştiriyor.
+	Date& set(int day, int mon, int year); //17	Tarihi değiştiriyor.
+
+	void print_date();
+	Date& gun_cikar(Date&, int)const;   //tarihten istenildiği kadar gün çıkaran üye fonksiyon
+	Date& gun_topla(Date&, int)const;  //tarihe istenildiği kadar gün ekleyen üye	fonksiyon
+	int number_of_leapyear()const;   //o yıla kadar kaç artık yıl geçtiğini hesaplayan üye fonksiyon
+	int month_to_date(int)const;	//yılın gününü hesaplayan üye fonksiyon
+
+	Date operator-(int day); //18	Tarihten gün çıkartan const üye operatör işlevi.
+
+
+
+	Date& operator+=(int day); //19	Tarihi gelen gün kadar arttıran üye operatör işlevi.Geri dönüş değeri* this 
+	Date& operator-=(int day); //20 Tarihi gelen gün kadar eksilten üye operatör işlevi.Geri dönüş değeri* this 
+	Date& operator++(); //21		Önek ++ operatörünü yükleyen işlev. 
+	Date operator++(int); //22		Sonek ++ operatörünü yükleyen işlev. 
+
+
+	Date& operator--(); //23	Önek -- operatörünü yükleyen işlev. 
+	Date operator--(int); //24	Sonek -- operatörünü yükleyen işlev. 
+
+	static Date random_date(); //25	Artık yıl testi yapan sınıfın static üye işlevi.
+
+
+
+	static constexpr bool isleap(int y); //26	Artık yıl testi yapan sınıfın static üye işlevi.
+
+	friend std::ostream& operator << (std::ostream& out, const Date::WeekDay& b);   //haftanın gününü yazdıran global operator işlevi
+
+	friend bool operator<(const Date&, const Date&);
+	friend bool operator==(const Date&, const Date&);
+
+
+	friend int operator-(const Date& d1, const Date& d2);
+
+	friend std::ostream& operator<<(std::ostream& os, const Date& date); //31
+	friend std::istream& operator>>(std::istream& is, Date& date); //32
+};
+
+bool operator<(const Date&, const Date&); //27	Date nesnelerinin karşılaştırılmasını sağlayacak global operatör işlevleri
+bool operator<=(const Date&, const Date&); //27
+bool operator>(const Date&, const Date&); //27
+bool operator>=(const Date&, const Date&); //27
+bool operator==(const Date&, const Date&); //27
+bool operator!=(const Date&, const Date&); //27
+//
+int operator-(const Date& d1, const Date& d2); //28	İki tarih arasındaki gün farkını döndüren global operatör işlevi
+Date operator+(const Date& date, int n); //29	Gelen tarihten n gün sonrasını döndüren global operatör işlevleri
+Date operator+(int n, const Date&); //29
+Date::WeekDay& operator++(Date::WeekDay& r); //30  //
+Date::WeekDay& operator++(Date::WeekDay& r, int); //30
+Date::WeekDay& operator--(Date::WeekDay& r); //30	
+Date::WeekDay& operator--(Date::WeekDay& r, int); //30
+//
+
+std::ostream& operator<<(std::ostream& os, const Date& date); //31		Date nesnelerinin değerlerini çıkış akımlarına yazdıracak global operatör işlevi (inserter) Formatlama şu şekilde: 31 Ekim 2019 Persembe
+std::istream& operator>>(std::istream& is, Date& date); //32	Date nesnelerine giriş akımlarından aldığı karakterlerden oluşturulacak değeri yerleştiren global operatör işlevi(extractor) Formatlama: gg / aa / yyyy
+#endif
